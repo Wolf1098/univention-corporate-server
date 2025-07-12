Wolf1098/univention-corporate-server

commit cbf79206dbb8737be9bcd3d8a934b1113004e4b3

feat(uldap): add optional strict type checking
Bug #58432

base/univention-python/modules/uldap.py
@@ -243,16 +243,21 @@ class access:
 
         self.__open(ca_certfile)
         if not self.base:
-            raise TypeError('base must at least be set to LDAP base')
-        if not self.base:
             log.error('No LDAP base given to uldap.access()! Can lead to mysterious errors! %s', ''.join(traceback.format_stack()))
             warnings.warn('No LDAP base given to uldap.access()! Can lead to mysterious errors!', DeprecationWarning, stacklevel=3)
-            self.base = ucr['ldap/base']
+
+            # a empty base is valid in one scenario: when wanting to detect the base via a certain search.
+            # but this can set any random base and set it to empty afterwards
+            # note: search(base='') will fallback to self.base
+            if ucr.is_true('directory/mananger/type-checking/strict'):
+                raise TypeError('base must at least be set to LDAP base')
 
     @property
     def authz_connection(self):
         log.error('Wrong access class in use! Use univention.admin.uldap instead of univention.uldap! %s', ''.join(traceback.format_stack()))
         warnings.warn('Wrong access class in use! Use univention.admin.uldap instead of univention.uldap!', DeprecationWarning, stacklevel=3)
+        if ucr.is_true('directory/mananger/type-checking/strict'):
+            raise TypeError('univention.admin.uldap required!')
         return self
 
     @_fix_reconnect_handling

commit 2bd06dd77c232c5ae2dc5ea9f342e3b9b639e0cf

fix(udm): fix typo in UCR variable name
Fixes: cbf79206dbb8737be9bcd3d8a934b1113004e4b3
Bug #58432

base/univention-python/modules/uldap.py
@@ -249,14 +249,14 @@ class access:
             # a empty base is valid in one scenario: when wanting to detect the base via a certain search.
             # but this can set any random base and set it to empty afterwards
             # note: search(base='') will fallback to self.base
-            if ucr.is_true('directory/mananger/type-checking/strict'):
+            if ucr.is_true('directory/manager/type-checking/strict'):
                 raise TypeError('base must at least be set to LDAP base')
 
     @property
     def authz_connection(self):
         log.error('Wrong access class in use! Use univention.admin.uldap instead of univention.uldap! %s', ''.join(traceback.format_stack()))
         warnings.warn('Wrong access class in use! Use univention.admin.uldap instead of univention.uldap!', DeprecationWarning, stacklevel=3)
-        if ucr.is_true('directory/mananger/type-checking/strict'):
+        if ucr.is_true('directory/manager/type-checking/strict'):
             raise TypeError('univention.admin.uldap required!')
         return self
 

management/univention-directory-manager-modules/modules/univention/admin/handlers/__init__.py
@@ -179,7 +179,7 @@ class simpleLdap:
             log.error('using univention.uldap.access instance is deprecated. Use univention.admin.uldap.access instead.')
             warnings.warn('using univention.uldap.access instance is deprecated. Use univention.admin.uldap.access instead.', DeprecationWarning, stacklevel=3)
             self.lo = univention.admin.uldap.access(lo=lo)
-            if configRegistry.is_true('directory/mananger/type-checking/strict'):
+            if configRegistry.is_true('directory/manager/type-checking/strict'):
                 raise TypeError('Expect univention.admin.uldap.access!')
         else:
             raise TypeError('lo must be instance of univention.admin.uldap.access.')
@@ -1934,7 +1934,7 @@ class simpleLdap:
         if isinstance(lo, univention.uldap.access):
             log.error('Wrong access class in use! Use univention.admin.uldap instead of univention.uldap! %s', ''.join(traceback.format_stack()))
             warnings.warn('Wrong access class in use! Use univention.admin.uldap instead of univention.uldap!', DeprecationWarning, stacklevel=3)
-            if configRegistry.is_true('directory/mananger/type-checking/strict'):
+            if configRegistry.is_true('directory/manager/type-checking/strict'):
                 raise TypeError('Expect univention.admin.uldap.access!')
 
         filter_e = cls.lookup_filter(filter_s, lo)

management/univention-directory-manager-modules/modules/univention/admin/modules.py
@@ -190,7 +190,7 @@ def init(lo: univention.admin.uldap.access, position: univention.admin.uldap.pos
     if isinstance(lo, univention.uldap.access):
         log.error('Wrong access class in use! Use univention.admin.uldap instead of univention.uldap! %s', ''.join(traceback.format_stack()))
         warnings.warn('Wrong access class in use! Use univention.admin.uldap instead of univention.uldap!', DeprecationWarning, stacklevel=3)
-        if configRegistry.is_true('directory/mananger/type-checking/strict'):
+        if configRegistry.is_true('directory/manager/type-checking/strict'):
             raise TypeError('Expect univention.admin.uldap.access!')
 
     # you better do a reload if init is called a second time

test/utils/utils.sh
@@ -94,7 +94,7 @@ basic_setup () {
 
 	# set strict mode for UDM so that the tests detect broken occurrences
 	# but customers / PS scripts aren't affected immediately
-	ucr set 'directory/mananger/type-checking/strict=true'
+	ucr set 'directory/manager/type-checking/strict=true'
 }
 
 stop_uss_and_restore_profile () {