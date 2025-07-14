cb177453fb98fc7bbfe1713429d3685098013701
Florian Best <best@univention.de>
2025-07-14 10:43:08 +0200

    fixup! fix(udm): fix typo in UCR variable name

--- base/univention-python/modules/uldap.py
+++ base/univention-python/modules/uldap.py
@@ -939,7 +939,7 @@ class access:
 
     def _handle_referral(self, exception: ldap.REFERRAL) -> ldap.ldapobject.ReconnectLDAPObject:
         """
-        Follow LDAP rederral.
+        Follow LDAP referral.
 
         :param ldap.REFERRAL exception: The LDAP referral exception.
         :returns: LDAP connection object for the referred LDAP server.
